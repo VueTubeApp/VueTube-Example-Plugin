72763c066f81ca7f6580b2ce2da5dc1bd17ac663
Kenny <27463495+Frontesque@users.noreply.github.com>
2022-04-04 17:55:11 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  manifest: {
+    name: "Test plugin", // Required
+    vuetube: "*",
+    version: "1.0", // Required
+    author: "Frontesque", // Required
+    description: "A plugin to test how vuetube handles plugins // Add 'Hello, World!' to the home page.", // Required
+    homepage: "https://vuetube.app",
+    license: "GPL-3.0" // Required
+  }
+}
